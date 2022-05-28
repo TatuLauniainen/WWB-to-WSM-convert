354d9827748d04460128297a688404521bcc2be7
ronikn <85103994+ronikn@users.noreply.github.com>
2022-05-28 22:54:52 +0300

    Added comments

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def main():
     now = datetime.datetime.now()
     date = (now.strftime("%Y%m%d-%H%M"))
 
-    frequencies = ["536125", "577875"]
+    frequencies = ["536125", "577875"]  # Later these frequencies will be read from the WWB inventory report
 
-    with open(f"{date}_frequencies.csv", 'w') as file:  # tälle joku hyvä tallennussijainti
+    with open(f"{date}_frequencies.csv", 'w') as file:  # Find a good folder to save this file to
         file.write("name;type;frequency;tolerance;minfrequency;maxfrequency;priority;squelchlevel\n")
         i = 1
         for frequency in frequencies:

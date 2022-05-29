b7495f4625dbf8538dd64860082ff1d50e7eb8c3
Tatu Launiainen <tatu.laun@gmail.com>
2022-05-30 01:35:26 +0300

    Added main.py functionality

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
+import modules.filemanager as filemanager, modules.parser as parser
 
-import datetime
 
+def main(WWB_path, WSM_path, WSM_folder):
 
-def main():
-
-    now = datetime.datetime.now()
-    date = (now.strftime("%Y%m%d-%H%M"))
-
-    frequencies = ["536125", "577875"]  # Later these frequencies will be read from the WWB inventory report
-
-    with open(f"{date}_frequencies.csv", 'w') as file:  # Find a good folder to save this file to
-        file.write("name;type;frequency;tolerance;minfrequency;maxfrequency;priority;squelchlevel\n")
-        i = 1
-        for frequency in frequencies:
-            file.write(f"Frequency {str(i).zfill(3)};2;{frequency};0;{frequency};{frequency};2;5\n")
-            i += 1
+    WWB_xml_root = filemanager.read_WWB_data(WWB_path)
+    frequencies = parser.parse_WWB_frequencies(WWB_xml_root)
+    filemanager.write_WSM_data(WSM_path, WSM_folder, frequencies)
 
 
 if __name__ == "__main__":
-    main()
+    WWB_path = "/Users/pvvmsktekniikka/Documents/VSCode/WWB-to-WSM-convert/dev_data/20220529.inv"
+    WSM_path = "/Users/pvvmsktekniikka/Documents/VSCode/WWB-to-WSM-convert/dev_data/20220529.wsm"
+    WSM_folder = "/Users/pvvmsktekniikka/Documents/VSCode/WWB-to-WSM-convert/dev_data/"
+    main(WWB_path, WSM_path, WSM_folder)

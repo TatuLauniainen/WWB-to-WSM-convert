5028c7906d6337be4f5fce072ea54f5485a813be
Tatu Launiainen <tatu.laun@gmail.com>
2022-05-30 00:25:46 +0300

    Added functionality to read WWB file

--- modules/filemanager.py
+++ modules/filemanager.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import xml.etree.ElementTree as ET
+
 
 def read_WWB_data(WWB_path):
     """
@@ -9,11 +11,14 @@ def read_WWB_data(WWB_path):
     WWB_path (str): path to WWB inventory file
 
     Returns:
-    WWB_data (str): data read from WWB inventory file
+    WWB_xml_root (Element): data read from WWB inventory file
 
     """
 
-    return
+    tree = ET.parse(WWB_path)
+    WWB_xml_root = tree.getroot()
+    return WWB_xml_root
+
 
 def write_WSM_data(WSM_path, WSM_data):
     """
@@ -25,20 +30,33 @@ def write_WSM_data(WSM_path, WSM_data):
     WSM_data (str): data writable to WSM file
 
     """
-    
+
     now = datetime.datetime.now()
-    date = (now.strftime("%Y%m%d-%H%M"))
+    date = now.strftime("%Y%m%d-%H%M")
 
-    frequencies = ["536125", "577875"]  # Later these frequencies will be read from the WWB inventory report
+    frequencies = [
+        "536125",
+        "577875",
+    ]  # Later these frequencies will be read from the WWB inventory report
 
-    with open(f"{date}_frequencies.csv", 'w') as file:  # Find a good folder to save this file to
-        file.write("name;type;frequency;tolerance;minfrequency;maxfrequency;priority;squelchlevel\n")
+    with open(
+        f"{date}_frequencies.csv", "w"
+    ) as file:  # Find a good folder to save this file to
+        file.write(
+            "name;type;frequency;tolerance;minfrequency;maxfrequency;priority;squelchlevel\n"
+        )
         i = 1
         for frequency in frequencies:
-            file.write(f"Frequency {str(i).zfill(3)};2;{frequency};0;{frequency};{frequency};2;5\n")
+            file.write(
+                f"Frequency {str(i).zfill(3)};2;{frequency};0;{frequency};{frequency};2;5\n"
+            )
             i += 1
     return
 
+
 if __name__ == "__main__":
-    frequencies = ["536125", "577875"]
-    write_WSM_data(frequencies)
+    # frequencies = ["536125", "577875"]
+    # write_WSM_data(frequencies)
+    read_WWB_data(
+        "/Users/pvvmsktekniikka/Documents/VSCode/WWB-to-WSM-convert/dev_data/20220528.inv"
+    )

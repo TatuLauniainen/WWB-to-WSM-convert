29589f7f5c24d5382fcefe7206dee67ad6de323c
Tatu Launiainen <tatu.laun@gmail.com>
2022-05-30 00:26:23 +0300

    Added parsing data from WWB format

--- modules/parser.py
+++ modules/parser.py
@@ -1,16 +1,37 @@
-"""
+import xml.etree.ElementTree as ET
+import filemanager
 
-Convert inventory data exported from Wireless Workbench
-to a format usable by Wireless System Manager
 
-Parameters:
-WWB_data (str): data exported from WWb
+def parse_WWB_frequencies(WWB_xml_root):
+    """
 
-Returns:
-WSM_data (str): data in WSM format
+    Find channel frequencies from Wireless Workbench inventory file and store thrm in a dict
 
-"""
+    Parameters:
+    WWB_xml_root (Element): data exported from WWB
 
-def convert_WWB_to_WSM():
+    Returns:
+    frequencies (dict): channel frequencies for all devices
+
+    """
+
+    frequencies = {}
+    for device in WWB_xml_root.iter("device"):
+
+        series = device.find("series").text
+        if not series in frequencies.keys():
+            frequencies[series] = []
+
+        for channel in device.iter("channel"):
+            frequencies[series].append(channel.find("frequency").text)
+
+    print(frequencies)
+
     return
 
+
+if __name__ == "__main__":
+    WWB_xml_root = filemanager.read_WWB_data(
+        "/Users/pvvmsktekniikka/Documents/VSCode/WWB-to-WSM-convert/dev_data/20220529.inv"
+    )
+    parse_WWB_frequencies(WWB_xml_root)

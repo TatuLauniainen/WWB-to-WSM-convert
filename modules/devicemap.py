3336535ac86b5eb8b4b9d94ab7614cc561a0c374
ronikn <85103994+ronikn@users.noreply.github.com>
2022-06-05 15:19:47 +0300

    Formatted devicemap.py dicts

--- modules/devicemap.py
+++ modules/devicemap.py
@@ -4,10 +4,12 @@ Map to convert from WWB device name to WSM device name
 
 
 def map_WWB_to_WSM(WWB_device):
-    WWB_to_WSM = {"EW 500 G3": "EM500G3", "EW 300 IEM G3": "SR-IEMG4"}
+    WWB_to_WSM = {"EW 500 G3": "EM500G3",
+                  "EW 300 IEM G3": "SR-IEMG4"}
     return WWB_to_WSM[WWB_device]
 
 
 def map_WSM_to_WWB(WSM_device):
-    WSM_to_WWB = {"EM500G3": "EW 500 G3", "SR-IEMG4": "EW 300 IEM G3"}
+    WSM_to_WWB = {"EM500G3": "EW 500 G3",
+                  "SR-IEMG4": "EW 300 IEM G3"}
     return WSM_to_WWB[WSM_device]

f17fd02df0c259f53a71fa38cfcef447d9b9bf48
Tatu Launiainen <tatu.laun@gmail.com>
2022-06-08 09:26:07 +0300

    Added ignore list to parser

--- modules/parser.py
+++ modules/parser.py
@@ -1,5 +1,10 @@
 import xml.etree.ElementTree as ET
 import modules.filemanager as filemanager
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter()
+
+ignore_devices = {"AXT600"}
 
 
 def parse_WWB_frequencies(WWB_xml_root):
@@ -18,20 +23,26 @@ def parse_WWB_frequencies(WWB_xml_root):
     frequencies = {}
     for device in WWB_xml_root.iter("device"):
 
+        model = device.find("model").text
+        if model in ignore_devices:
+            print(model + " ignored")
+            continue
+
         series = device.find("series").text
         if not series in frequencies.keys():
             frequencies[series] = []
 
         for channel in device.iter("channel"):
+            print(channel.find("frequency"))
             frequencies[series].append(channel.find("frequency").text)
 
-    print(frequencies)
+    pp.pprint(frequencies)
 
     return frequencies
 
 
 if __name__ == "__main__":
     WWB_xml_root = filemanager.read_WWB_data(
-        "/Users/pvvmsktekniikka/Documents/VSCode/WWB-to-WSM-convert/dev_data/20220529.inv"
+        "/Users/pvvmsktekniikka/Documents/VSCode/WWB-to-WSM-convert/dev_data/20220607.inv"
     )
     parse_WWB_frequencies(WWB_xml_root)
